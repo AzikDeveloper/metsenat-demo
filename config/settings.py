9fbadd5a9711fb46b7582f39a3121eb8211c81d0
AzikDeveloper <78961300+AzikDeveloper@users.noreply.github.com>
2021-12-15 23:43:50 +0500

    session del

--- config/settings.py
+++ config/settings.py
@@ -43,8 +43,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100
